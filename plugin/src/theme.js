0378f5a146f31a598aefb75d88cbc713fc6c8030
Benedicte Raae <raae@raae.codes>
2022-03-10 22:03:40 +0100

    fix: responsive typo not applied

--- plugin/src/theme.js
+++ plugin/src/theme.js
@@ -2,6 +2,6 @@ import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 import config from "./config";
 
 let theme = createTheme(config);
-responsiveFontSizes(theme);
+theme = responsiveFontSizes(theme);
 
 export default theme;
